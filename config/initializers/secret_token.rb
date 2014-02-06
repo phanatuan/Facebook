9b7829d45229fd198f0b86e8bb1eba916535c9e6
Phan Anh Tuan <phananhtuan1011@gmail.com>
2014-02-06 12:45:29 +0700

    Treehouse Facebook created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facebook::Application.config.secret_key_base = '43eceecb0eedf23c71359e37b21032fd06b1cc1bd2d8ecd5d2179d1fe6fb34253bc515faaabf0a9f6584ea8bf3955c5856f6e8b0f9f5875ebf1d8392dcfb51f2'
